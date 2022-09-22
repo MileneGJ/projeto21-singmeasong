3bfdd3c0b1a299cf4d716e02078a137164781667
milenegj <milene.jannetti@gmail.com>
2022-09-22 18:26:44 -0500

    Unit tests: update recommendation scores

--- back-end/jest.config.js
+++ back-end/jest.config.js
@@ -8,6 +8,18 @@ export default {
       useESM: true,
     },
   },
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'test-config',
+    'interfaces',
+    'repositories',
+    'jestGlobalMocks.ts',
+    '<rootDir>/src/server.ts',
+    '<rootDir>/src/utils',
+    '<rootDir>/src/config',
+    '<rootDir>/tests/factories'
+  ],
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
