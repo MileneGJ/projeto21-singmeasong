6c724c15739ec029277c0f8d495ac274847727a9
milenegj <milene.jannetti@gmail.com>
2022-09-24 16:35:09 -0500

    E2e tests: up or downvoting a recommendation

--- front-end/cypress/e2e/recommendations.cy.js
+++ front-end/cypress/e2e/recommendations.cy.js
@@ -1,11 +1,14 @@
 import invalidRecommendationFactory from "./factories/invalidRecommendationFactory"
 import recommendationFactory from "./factories/recommendationFactory"
 
+
 beforeEach(() => {
   cy.request('POST', 'http://localhost:5000/database-reset')
 })
 
+
 describe('Tests for adding a new recommendation', () => {
+
 
   it('Add recommendation successfully when inputs are valid', () => {
     const recommendation = recommendationFactory()
@@ -25,7 +28,6 @@ describe('Tests for adding a new recommendation', () => {
 
     cy.contains(recommendation.name).should("be.visible");
     cy.get('article').its('length').should("be.gt", initialLength)
-
   })
 
 
@@ -33,25 +35,25 @@ describe('Tests for adding a new recommendation', () => {
     const recommendation = recommendationFactory()
 
     cy.intercept("GET", "/recommendations").as("getRecommendation");
-
     cy.visit("http://localhost:3000");
     cy.request("POST", "http://localhost:5000/recommendations", recommendation)
-    cy.wait("@getRecommendation");
+      .then(() => {
+        cy.wait("@getRecommendation");
 
-    cy.get('article')
-      .then(elements => {
-        const initialLength = elements.length
-        cy.get('input[placeholder=Name]').should('be.visible').type(recommendation.name);
-        cy.get("input[placeholder='https://youtu.be/...'").should('be.visible').type(recommendation.youtubeLink);
+        cy.get('article')
+          .then(elements => {
+            const initialLength = elements.length
+            cy.get('input[placeholder=Name]').should('be.visible').type(recommendation.name);
+            cy.get("input[placeholder='https://youtu.be/...'").should('be.visible').type(recommendation.youtubeLink);
 
-        cy.intercept("POST", "/recommendations").as("postRecommendation");
-        cy.get("button").should('be.visible').click();
-        cy.wait("@postRecommendation");
+            cy.intercept("POST", "/recommendations").as("postRecommendation");
+            cy.get("button").should('be.visible').click();
+            cy.wait("@postRecommendation");
 
-        cy.contains(recommendation.name).its('length').should("eq", 1);
-        cy.get('article').its('length').should("eq", initialLength)
+            cy.contains(recommendation.name).its('length').should("eq", 1);
+            cy.get('article').its('length').should("eq", initialLength)
+          })
       })
-
   })
 
 
@@ -70,7 +72,92 @@ describe('Tests for adding a new recommendation', () => {
     cy.wait("@postRecommendation");
 
     cy.contains(recommendation.name).should('not.exist')
-
   })
+
+
+})
+
+
+describe('Tests for upvoting or downvoting recommendation', () => {
+
+  it('Increases score in one point when upvoting recommendation', () => {
+    const recommendation = recommendationFactory()
+
+    cy.intercept("GET", "/recommendations").as("getRecommendation");
+    cy.visit("http://localhost:3000");
+    cy.request("POST", "http://localhost:5000/recommendations", recommendation)
+      .then(() => {
+        cy.wait("@getRecommendation");
+
+        cy.contains(recommendation.name).parent().children('div').last().should(div => expect(div).to.contain('0'))
+
+        cy.intercept("POST", "/recommendations/1/upvote").as("upvoteRecommendation");
+        cy.contains(recommendation.name).parent().children('div').last().children('svg').first().should('be.visible').click();
+
+        cy.wait("@upvoteRecommendation");
+        cy.wait("@getRecommendation");
+
+        cy.contains(recommendation.name).parent().children('div').last().should(div => expect(div).to.contain('1'))
+      })
+  })
+
+
+  it('Decreases score in one point when downvoting recommendation', () => {
+    const recommendation = recommendationFactory()
+
+    cy.intercept("GET", "/recommendations").as("getRecommendation");
+    cy.visit("http://localhost:3000");
+    cy.request("POST", "http://localhost:5000/recommendations", recommendation)
+      .then(() => {
+        cy.wait("@getRecommendation");
+
+        cy.contains(recommendation.name).parent().children('div').last().should(div => expect(div).to.contain('0'))
+
+        cy.intercept("POST", "/recommendations/1/downvote").as("downvoteRecommendation");
+        cy.contains(recommendation.name).parent().children('div').last().children('svg').last().should('be.visible').click();
+
+        cy.wait("@downvoteRecommendation");
+        cy.wait("@getRecommendation");
+
+        cy.contains(recommendation.name).parent().children('div').last().should(div => expect(div).to.contain('-1'))
+      })
+  })
+
+  it('Deletes recommendation when downvoting it to less than -5', () => {
+    const recommendation = recommendationFactory()
+
+    cy.request("POST", "http://localhost:5000/recommendations", recommendation)
+      .then(() => {
+        cy.request("POST", "http://localhost:5000/recommendations/1/downvote")
+          .then(() => {
+            cy.request("POST", "http://localhost:5000/recommendations/1/downvote")
+              .then(() => {
+                cy.request("POST", "http://localhost:5000/recommendations/1/downvote")
+                  .then(() => {
+                    cy.request("POST", "http://localhost:5000/recommendations/1/downvote")
+                      .then(() => {
+                        cy.request("POST", "http://localhost:5000/recommendations/1/downvote")
+                          .then(() => {
+                            cy.intercept("GET", "/recommendations").as("getRecommendation");
+                            cy.visit("http://localhost:3000");
+                            cy.wait("@getRecommendation");
+
+                            cy.contains(recommendation.name).parent().children('div').last().should(div => expect(div).to.contain('-5'))
+
+                            cy.intercept("POST", "/recommendations/1/downvote").as("downvoteRecommendation");
+                            cy.contains(recommendation.name).parent().children('div').last().children('svg').last().should('be.visible').click();
+
+                            cy.wait("@downvoteRecommendation");
+                            cy.wait("@getRecommendation");
+
+                            cy.contains(recommendation.name).should('not.exist')
+                          })
+                      })
+                  })
+              })
+          })
+      })
+  })
+
 
 })
